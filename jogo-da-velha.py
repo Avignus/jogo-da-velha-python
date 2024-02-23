9ef941bb3bc08b1a335c3e6f30f01f5a4b882364
Igor <igor.rapozo@gmail.com>
2024-02-23 01:46:00 -0300

    making match wins logic

--- jogo-da-velha.py
+++ jogo-da-velha.py
@@ -1,7 +1,8 @@
-class Jogador:
-    def __init__(self, nome):
-        self.nome = nome
-        self.pontos = 0
+class Player:
+    def __init__(self, name, sign):
+        self.name = name
+        self.points = 0
+        self.sign = sign
 
 
 class MathAroundTheGame:
@@ -14,62 +15,85 @@ class MathAroundTheGame:
                 result[element].append(i)
         return result
 
+    def check_if_it_has_won_pattern(self):
+        pass
+
 
 math_around_the_game = MathAroundTheGame()
 
 
-class Jogo:
-    def __init__(self, jogadores):
-        self.jogadores = jogadores
+class Game:
+    def __init__(self, players):
+        self.players = players
         self.fim = False
         self.game = {str(num): "_" for num in range(1, 10)}
         self.turn = 0
-        self.list_of_wins = [[0, 1, 2], [0, 4, 8],
+        self.list_of_wins = [[0, 1, 2], [0, 3, 6], [0, 4, 8],
                              [3, 4, 5], [4, 5, 6], [2, 4, 6]]
         self.occurrences = []
+        self.signs = ["X", "O"]
 
-    def iniciar_jogo(self):
+    def show_board(self):
+        skip = 0
+        for num in self.game.keys():
+            if (int(num) % 3 == 0):
+                if skip == 0:
+                    print(list(self.game.values())[:int(num)])
+                    skip += 3
+                else:
+                    print(list(self.game.values())[skip:int(num)])
+                    skip += 3
+
+    def show_positions_available(self):
+        positions_available = [i for i, j in self.game.items() if j == "_"]
+        print(f"Posições disponíveis: {positions_available}")
+
+    def play_game(self):
         while self.fim == False:
-            skip = 0
-            for num in self.game.keys():
-                if (int(num) % 3 == 0):
-                    if skip == 0:
-                        print(list(self.game.values())[:int(num)])
-                        skip += 3
-                    else:
-                        print(list(self.game.values())[skip:int(num)])
-                        skip += 3
-            positions_available = [i for i, j in self.game.items() if j == "_"]
+            self.show_board()
+            self.show_positions_available()
+            self.play_turn()
 
-            print(f"posições disponíveis: {positions_available}")
-            if (self.turn == 0):
-                position_selected = input(
-                    f"Selecione a posição, {self.jogadores[0].nome}\n")
-                self.game[position_selected] = "X"
-                self.occurences = math_around_the_game.count_occurrences_with_index(
-                    list(self.game.values()))
-                print(self.occurences)
-                if (list(self.occurences["X"]) in self.list_of_wins):
-                    self.jogadores[0].pontos += 1
-                    print(f"O jogador {self.jogadores[0].nome} ganhou!")
+    def play_turn(self):
+        player = self.players[self.turn]
+        sign = player.sign
+        position_selected = input(
+            f"É a vez de {player.name}, selecione a posicao:\n")
+        if self.game[position_selected] == "_":
+            self.game[position_selected] = sign
+            self.occurrences = math_around_the_game.count_occurrences_with_index(
+                list(self.game.values()))
+            # print(self.occurrences[sign])
+            # print(self.list_of_wins)
+            for i in self.list_of_wins:
+                print(i)
+                print(list(self.occurrences[sign]))
+                if (i in list(self.occurrences[sign])):
+                    player.points += 1
+                    print(f"O jogador {player.name} ganhou!")
+                    self.show_board()
                     self.fim = True
+                    break
+            self.change_turn()
+            # print(i)
+            # if (list(self.occurrences[sign]) in self.list_of_wins):
+            #     player.points += 1
+            #     print(f"O jogador {player.name} ganhou!")
+            #     self.show_board()
+            #     self.fim = True
+            # else:
+            #     self.change_turn()
+        else:
+            print("Posição já selecionada, por favor, selecione outra:")
 
-                self.turn = 1
-            elif (self.turn == 1):
-                position_selected = input(
-                    f"Selecione a posição, {self.jogadores[1].nome}\n")
-                self.game[position_selected] = "O"
-                self.occurences = math_around_the_game.count_occurrences_with_index(
-                    list(self.game.values()))
-                print(self.occurences)
-                if (list(self.occurences["O"]) in self.list_of_wins):
-                    self.jogadores[1].pontos += 1
-                    print(f"O jogador {self.jogadores[0].nome} ganhou!")
-                    self.fim = True
-                self.turn = 0
+    def change_turn(self):
+        if self.turn == 0:
+            self.turn = 1
+        else:
+            self.turn = 0
 
 
-jogadores = [Jogador("Igor"), Jogador("Henrique")]
-jogo = Jogo(jogadores)
+players = [Player("Igor", "X"), Player("Henrique", "O")]
+game = Game(players)
 
-jogo.iniciar_jogo()
+game.play_game()
